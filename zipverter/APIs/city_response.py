919bb6d9724cd64addc657fdd3619e40b9af882e
asholok <asholok@gmail.com>
2015-06-26 19:32:15 +0300

    manage city_response.py

--- zipverter/APIs/city_response.py
+++ zipverter/APIs/city_response.py
@@ -3,6 +3,7 @@ from handler.models import LocationTable
 from handler.lazy_load import find_city
 
 class ZipTableResource(resources.ModelResource):
+
     class Meta:
         queryset = LocationTable.objects.all()
         resource_name = 'zip_table'
@@ -10,25 +11,25 @@ class ZipTableResource(resources.ModelResource):
         allowed_methods = ['get', 'post']
         always_return_data = True
 
-    def __create_location(self, zip_code, country):
-        city = find_city(zip_code, country)
-
-        if city:
-            obj = LocationTable(country=country, zip_code=zip_code, city=city)
-            obj.save()
-
-        else:
-            pass
-
-
-    def dehydrate(self, bundle):
+    def obj_create(self, bundle, request=None, **kwargs):
+        response = {}
         zip_code = bundle.data['zip']
         country = bundle.data['country']
         filtered = LocationTable.objects.filter(country=country, zip_code=zip_code)
         
         if filtered:
-            Meta.queryset = filtered
-        else:
-            self.create_location()
+            response['city'] = filtered[0]
+        else:    
+            city = find_city(zip_code, country)
+            
+            if city:
+                obj = LocationTable(country=country, zip_code=zip_code, city=city)
+                obj.save()
+                response['city'] = city
+            else: 
+                response['errore'] = 'Country and zip code missmatch'
+
+        return self.create_response(request, response, None, **kwargs)
+
 
 
