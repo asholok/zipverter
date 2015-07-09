937ecf8917020b880051dbdda6cf34bb13ff849e
asholok <asholok@gmail.com>
2015-07-09 11:40:31 +0300

    add django whitenoise

--- zipverter/wsgi.py
+++ zipverter/wsgi.py
@@ -1,5 +1,7 @@
 import os 
-from django.core.handlers.wsgi import WSGIHandler
+from django.core.wsgi import get_wsgi_application
+from whitenoise.dgango import DjangoWhiteNoise
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "zipverter.settings"
-application = WSGIHandler()
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
