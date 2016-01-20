82ecfc226818835ca15d6284821f99803cc11fb7
asholok <asholok@gmail.com>
2016-01-20 14:07:21 +0200

    fix Mexico cities

--- zipverter/handler/lazy_load.py
+++ zipverter/handler/lazy_load.py
@@ -2,6 +2,7 @@ from cities.models import City, PostalCode, Country, District
 import requests
 import json
 
+
 def formatize_results(obj):
     return {'state': obj.region.name, 'state_code': obj.region.code, 'city': obj.name}
 
@@ -23,7 +24,14 @@ def find_location_info(zip_code, country_name):
     if country_name == 'Brazil':
         return get_postmon_results(zip_code)
     zip_objs = PostalCode.objects.filter(code=zip_code, country__name=country_name)
+
     if zip_objs:
+        if country_name == 'Mexico':
+            try:
+                names = zip_objs[0].names
+                return {'state': names[1], 'state_code': '', 'city': names[3]}
+            except:
+                return None
         return formatize_results(City.objects.distance(zip_objs[0].location).order_by('distance')[0])
     return None
     
