6aecde6060bcefc0ecab9ad17dca97f184651add
asholok <asholok@gmail.com>
2016-05-17 23:15:47 +0300

    add populatoin oreder

--- zipverter/handler/helper.py
+++ zipverter/handler/helper.py
@@ -7,11 +7,13 @@ def get_city(city_name, country_name):
     try:
         city_filter = Q(name=city_name, country__name=country_name)
         return City.objects.get(city_filter)
-    except Exception, City.DoesNotExist:
+    except City.DoesNotExist:
         city_filter = Q(name_std=city_name, country__name=country_name)
         return City.objects.get(city_filter)
-    except Exception, City.MultipleObjectsReturned:
-        return City.objects.filter(city_filter)[0]
+    except City.MultipleObjectsReturned:
+        return City.objects.filter(city_filter).order_by('-population')[0]
+    except Exception, e:
+        pass
     return False
 
 def get_cities_neighbor(city_name, country_name, measurement, radius):
